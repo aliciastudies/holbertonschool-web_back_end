222a827f4dc3dabf127109d310139464fb7b9698
aliciastudies <6722@holbertonstudents.com>
2024-01-08 12:29:29 +1100

    fix: added collection type List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ type-annotated function list of floats """
+from typing import List
 
 
-def sum_list(input_list: float) -> str:
+def sum_list(input_list: List[float]) -> float:
     """
     returns float
     """
