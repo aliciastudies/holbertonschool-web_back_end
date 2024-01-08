93c795bef6f23e1e9191b0e87dfa3a9ad029a76e
aliciastudies <6722@holbertonstudents.com>
2024-01-08 12:36:00 +1100

    fix: added Optional collection type

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ type-annotated function list of floats """
-from typing import List, Union
+from typing import List, Union, Optional
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
