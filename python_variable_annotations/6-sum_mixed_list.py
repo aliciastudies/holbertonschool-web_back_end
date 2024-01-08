54f88868a0eaf8b855291b7919ce0c69b8ce4210
aliciastudies <6722@holbertonstudents.com>
2024-01-08 12:38:29 +1100

    fix: swapped type order in List to match output

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" type-annotated function list of floats """
+""" type-annotated function list of mixed """
 from typing import List, Union, Optional
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     returns float
     """
