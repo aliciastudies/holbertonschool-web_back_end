e984d5f233b9a3d83619f2eace2e4abca178274c
aliciastudies <6722@holbertonstudents.com>
2024-01-09 13:58:19 +1100

    fix: annotated return value

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ asynchronous coroutine """
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     returns float a random number between 0 and 10
     """
