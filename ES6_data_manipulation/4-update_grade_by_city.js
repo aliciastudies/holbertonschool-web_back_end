7d3be2a1a3dc642a0e658554dc346c4d2f72dbdc
aliciastudies <6722@holbertonstudents.com>
2024-02-01 17:28:54 +1100

    feat: combine

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  const filteredStudents = studentList.filter(
+    (student) => student.location === city
+  );
+
+  const updatedStudents = filteredStudents.map((student) => {
+    const matchingGrade = newGrades.find(
+      (grade) => grade.studentId === student.id
+    );
+
+    return { ...student, grade: matchingGrade ? matchingGrade.grade : 'N/A' };
+  });
+
+  return updatedStudents;
+}
