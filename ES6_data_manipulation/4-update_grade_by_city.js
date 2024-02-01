4272f4b58d582c9121c93b4b43b1a05ab1df481b
aliciastudies <6722@holbertonstudents.com>
2024-02-01 17:31:11 +1100

    fix: added trailing commas to pass linter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
   const filteredStudents = studentList.filter(
-    (student) => student.location === city
+    (student) => student.location === city,
   );
 
   const updatedStudents = filteredStudents.map((student) => {
     const matchingGrade = newGrades.find(
-      (grade) => grade.studentId === student.id
+      (grade) => grade.studentId === student.id,
     );
 
     return { ...student, grade: matchingGrade ? matchingGrade.grade : 'N/A' };
